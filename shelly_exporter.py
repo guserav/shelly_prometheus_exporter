27d26c91ddd8dafef56e22f1aa243a4f2d8fc83b
guserav <guserav@users.noreply.github.com>
2024-07-20 00:27:58 +0200

    Add rest of easy metrics for switches

--- shelly_exporter.py
+++ shelly_exporter.py
@@ -2,10 +2,10 @@ import argparse
 from collections.abc import Callable, Collection
 import dataclasses
 import prometheus_client
-from prometheus_client import Gauge
+from prometheus_client import Gauge, Enum
 import requests
 import time
-from typing import Any
+from typing import Any, Union
 
 
 METRICS = {}
@@ -31,18 +31,73 @@ class CommonLabels():
         return getattr(self, key)
 
 
-def get_or_create_metric(metric_class: Callable, name: str, description: str, extra_labels: Collection=[]) -> None:
+def get_or_create_metric(metric_class: Callable, name: str, description: str, extra_labels: Collection=[], **kwargs) -> None:
     if name not in METRICS:
-        METRICS[name] = metric_class(name, description, CommonLabels.get_label_names() + extra_labels)
+        METRICS[name] = metric_class(name, description, CommonLabels.get_label_names() + extra_labels, **kwargs)
     return METRICS[name]
+
+def lookup(source, path: list[str]):
+    if not path:
+        return source
+    if "." in path:
+        path = path.split(".")
+    return lookup(source[path[0]], path[1:])
+
+@dataclasses.dataclass
+class Metric:
+    metric_name: str
+    metric_path: list[str] = dataclasses.field(default_factory=list)
+
+    def __post_init__(self):
+        if not self.metric_path:
+            self.metric_path = [self.metric_name]
+
+    def parse_from_data(self, common_labels: CommonLabels, data: dict, component_name: str) -> None:
+        raise NotImplementedError("This shouldn't be called")
+
+
+@dataclasses.dataclass
+class MetricGauge(Metric):
+    def parse_from_data(self, common_labels: CommonLabels, data: dict, component_name: str) -> None:
+        gauge: Gauge = get_or_create_metric(
+                Gauge,
+                f"shelly_{component_name}_{self.metric_name}",
+                f"Shelly {component_name} {self.metric_name} gauge")
+        gauge.labels(**common_labels.as_dict()).set(lookup(data, self.metric_path))
+
+
+@dataclasses.dataclass
+class MetricEnum(Metric):
+    states: list[str] = dataclasses.field(default_factory=list)
+
+    def __post_init__(self):
+        super().__post_init__()
+        if not self.states:
+            raise ValueError("States needs to be a non empty list")
+
+    def parse_from_data(self, common_labels: CommonLabels, data: dict, component_name: str) -> None:
+        enum: Enum = get_or_create_metric(
+                Enum,
+                f"shelly_{component_name}_{self.metric_name}",
+                f"Shelly {component_name} {self.metric_name} Enum",
+                states=self.states)
+        val = lookup(data, self.metric_path)
+        if isinstance(val, bool):
+            val = self.states[1 if val else 0]
+        enum.labels(**common_labels.as_dict()).state(val)
 
 
 def parse_switch(common_labels: CommonLabels, data: dict):
-    # TODO ("temperature", "tC")
-    # TODO ("aenergy", "total")
-    for gauge_name in ["voltage", "current", "apower", ]:
-        gauge: Gauge = get_or_create_metric(Gauge, f"shelly_switch_{gauge_name}", f"Shelly switch {gauge_name} gauge")
-        gauge.labels(**common_labels.as_dict()).set(data[gauge_name])
+    metrics = [
+            MetricGauge("voltage"),
+            MetricGauge("current"),
+            MetricGauge("apower"),
+            MetricGauge("temperature", ["temperature", "tC"]),
+            MetricGauge("energy_consumed", ["aenergy", "total"]),
+            MetricEnum("output", states=["Off", "On"]),
+    ]
+    for metric in metrics:
+        metric.parse_from_data(common_labels, data, "switch")
 
 
 def collect(ip: str):
